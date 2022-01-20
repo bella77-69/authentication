51ca133759835a8ede6dc96cbf9af6c4b986efa9
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-19 20:33:33 -0800

    installed body-parser, added post route in server.js

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
- 
-  })
+})
 
-  app.listen(process.env.PORT || 5000, function (err) {
+app.post('/quotes', (req, res) => {
+    console.log('Hellooooooooooooooooo!')
+})
+
+app.listen(process.env.PORT || 5000, function (err) {
   if (err) {
     console.log(err);
   } else {
