8fbc26b4919642f17de9be9f1e8e08c5b791d111
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-19 21:13:00 -0800

    installed ejs, added views folder

--- server.js
+++ server.js
@@ -1,19 +1,52 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MongoClient = require("mongodb").MongoClient;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+MongoClient.connect(
+  "mongodb+srv://test:test@cluster0.abdec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useUnifiedTopology: true }
+).then((client) => {
+  console.log("Connected to Database");
 
-app.post('/quotes', (req, res) => {
-    console.log('Hellooooooooooooooooo!')
-})
+  const db = client.db("star-wars-quotes");
 
-app.listen(process.env.PORT || 5000, function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Server started at PORT ${PORT}`);
-  }
+  const quotesCollection = db.collection('quotes');
+
+  app.set('view engine', 'ejs')
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray()
+      .then(results => {
+        res.render('index.ejs', { quotes: results })
+      })
+      .catch(/* ... */)
+  })
+  
+  app.get("/", (req, res) => {
+    db.collection('quotes').find().toArray()
+    .then(results => {
+      console.log(results)
+    })
+    .catch(error => console.error(error))
+  });
+
+  app.post('/quotes', (req, res) => {
+    quotesCollection.insertOne(req.body)
+      .then(result => {
+        res.redirect('/')
+      })
+      .catch(error => console.error(error))
+  });
+
+  app.listen(process.env.PORT || 5000, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Server started at PORT ${PORT}`);
+    }
+  });
 });
